40867fce723fb09e9e1a8938d702ec43d7723476
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-07-27 00:26:56 +0700

    feat: Подготовить файлы проекта

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { PORT = 3000 } = process.env;
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.status(404).send('<h1>Страница не найдена</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
