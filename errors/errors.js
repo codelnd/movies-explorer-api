8d3bcbe1399649c53fa882f0069d56752d7ee322
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-08-05 02:04:20 +0700

    hotfix: Сделать правки в errors

--- errors/errors.js
+++ errors/errors.js
@@ -1,25 +1,11 @@
 const NotFoundError = require('./NotFoundError');
-const ConflictError = require('./ConflictError');
-const BadRequestError = require('./BadRequestError');
 
 module.exports.handleErrors = (err, req, res, next) => {
-  if (err.name === 'CastError' || err.name === 'ValidationError') {
-    const error = new BadRequestError();
-    res.status(error.statusCode).send({ message: error.message });
-    return;
-  }
-  if (err.code === 11000) {
-    const error = new ConflictError();
-    res.status(error.statusCode).send({ message: error.message });
-    return;
-  }
+  const status = err.statusCode || 500;
+  const { message } = err;
 
-  const { statusCode = 500, message } = err;
-
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка!' : message,
-  });
-  next();
+  res.status(status).json({ err: message || 'На сервере произошла ошибка.' });
+  return next();
 };
 
 module.exports.notFoundPage = (req, res, next) => {
