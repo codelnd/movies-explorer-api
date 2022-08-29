117d25d4bacceaa970cce8d41a941d88ae77bf85
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-08-29 19:23:03 +0700

    feat: Дополнить массив доменов CORS

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,10 @@ module.exports.DEV_KEY = 'magic-secret-key';
 module.exports.DEV_MONGO_URL = 'mongodb://localhost:27017/moviesdb';
 
 module.exports.allowedCors = [
-  'https://findmovies.nomoredomains.xyz',
   'http://localhost:3000',
+  'localhost:3000',
   'http://localhost:3001',
+  'localhost:3001',
+  'https://findmovies.nomoredomains.xyz',
+  'http://findmovies.nomoredomains.xyz',
 ];
